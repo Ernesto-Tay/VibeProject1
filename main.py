f8da20d03360888a4da2e4f9202eb98789226b53
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-09-28 18:59:05 -0600

    Primer entry y botón de acción

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 root = tk.Tk()
 root.title("Mi primera App en tkinter")
 root.geometry("400x400")
 root.resizable(False, False)
 
-main = ttk.Frame(root, padding="5")
-main.grid(column=0, row=0)
-root.rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
+main = ttk.Frame(root, padding=15)
+main.pack(side="top", fill="x")
+def button1():
+    top = tk.Toplevel(main)
+    top.title("Nuevo")
+    top.resizable(False, False)
+    ttk.Label(top, text="Nombre").grid(row=0, column=0)
+    entry1 = ttk.Entry(top)
+    entry1.grid(row=0, column=1)
+    def retornar():
+        text1=entry1.get()
+        ttk.Label(top, text=f"Tu nombre es: {text1}").grid(row=0, column=2)
+    ttk.Button(top, text="Ingresar", command=retornar).grid(row=0, column=3)
 
-root.nombre = tk.StringVar(value="WEBOS")
+def button2():
+    pass
 
-ttk.Label(main, textvariable=root.nombre).grid(column=0, row=0, sticky="w")
+ttk.Button(main, text="Nuevo", command=button1).pack(side="left")
+ttk.Button(main, text="Entrar", command = button2).pack(side="left")
+ttk.Button(main, text="Salir", command=root.destroy).pack(side="right")
+entry = ttk.Entry(main)
 
 
 root.mainloop()
